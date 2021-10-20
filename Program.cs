c34be533b20d8ab1b2ed6d45982d86b0f0a1d2ec
Chrisfp27 <92868384+Chrisfp27@users.noreply.github.com>
2021-10-20 09:43:27 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Evaluacion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int a = 4;
+         int b = 7;
+         int c = 2;
+
+          Console.WriteLine ("Combinacion de variables"); 
+
+          Console.WriteLine ("a = "+ a); Console.WriteLine ("b = "+b); Console.WriteLine ("c = "+c);
+
+          Console.WriteLine ("");
+
+           //aacb
+           Console.WriteLine ("Combinacion aacb");
+           Console.Write (a);
+           Console.Write (a);
+           Console.Write (c);
+           Console.Write (b);
+           Console.WriteLine ("");
+           Console.WriteLine ("");
+
+           //bca
+           Console.WriteLine ("Combinacion bca");
+           Console.Write (b);
+           Console.Write (c);
+           Console.Write (a);
+           Console.WriteLine ("");
+           Console.WriteLine ("");
+
+           //bcbc
+           Console.WriteLine ("Combinacion bcbc");
+           Console.Write (b);
+           Console.Write (c);
+           Console.Write (b);
+           Console.Write (c);
+
+
+    
+
+
+          
+
+        
+
+        
+
+          
+
+
+
+        }
+    }
+}
